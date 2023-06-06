def3dceead21f9f669962d2da402271a89bc09cf
move7 <addouni.fatima@gmail.com>
2023-06-06 12:56:42 -0700

    tasks

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = int(str(number)[-1])
+message = f"Last digit of {number} is {last_digit} and is "
+if(last_digit < 6):
+    message += "less than 6 and not 0"
+elif(last_digit > 5):
+    message += "greater than 5"
+else:
+    message += "0"
+print(message)
---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+result = "0, "
+for i in range(1, 100):
+    if(i % 15 == 0):
+        result += "FizzBuzz"
+    elif(i % 5 == 0):
+        result += "Fizz"
+    elif(i % 3 == 0):
+        result += "Buzz"
+    else:
+        result += str(i)
+    result += ", "
+print(result + "Buzz", end='')
