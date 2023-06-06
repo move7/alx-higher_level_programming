34222e1529e92004d77f1741a8db3ef87474a5c8
move7 <addouni.fatima@gmail.com>
2023-06-06 15:51:03 -0700

    task12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,9 +4,9 @@ for i in range(1, 100):
     if(i % 15 == 0):
         result += "FizzBuzz"
     elif(i % 5 == 0):
-        result += "Fizz"
-    elif(i % 3 == 0):
         result += "Buzz"
+    elif(i % 3 == 0):
+        result += "Fizz"
     else:
         result += str(i)
     result += " "
