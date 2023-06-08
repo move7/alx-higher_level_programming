6f1cf5a7a0fbc882dde830fe78763d85d439d79f
move7 <addouni.fatima@gmail.com>
2023-06-08 07:10:40 -0700

    task2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-for i in argv:
-    sum += int(i)
-print("{}".format(sum))
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    length = len(sys.argv) - 1
+    for i in range(length):
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
