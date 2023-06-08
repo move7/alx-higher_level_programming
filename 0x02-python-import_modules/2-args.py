e7fc2fa5312c14de0778e7eae6efded547615b31
move7 <addouni.fatima@gmail.com>
2023-06-08 06:54:57 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
         print("{} arguments:".format(length))
     else:
         print("{} argument:".format(length))
-    for i in  range(length):
+    for i in range(length):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
