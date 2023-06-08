6e8cc185e828697586086ce8ec2c370e4dad44a1
move7 <addouni.fatima@gmail.com>
2023-06-08 16:36:14 -0700

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import sys
 import hidden_4
 if __name__ == "__main__":
-    list_names = dir(hidden4)
-    for name in list_name:
+    list_names = dir(hidden_4)
+    for name in list_names:
         if name[:2] != "__":
             print("{}".format(name))
