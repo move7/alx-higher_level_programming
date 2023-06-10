f21a20cc56748523e050de1b617c82556281281e
move7 <addouni.fatima@gmail.com>
2023-06-10 06:19:55 -0700

    task3

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for n in row:
-            print("{:d}".format(n), end=" ")
+            print("{:d}".format(n), end=" " if n != row[-1] else "")
         print()
