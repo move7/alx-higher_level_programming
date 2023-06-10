84550de47ec19e3dfed1f8a956df7e9671e2bb5e
move7 <addouni.fatima@gmail.com>
2023-06-10 13:21:21 -0700

    task13

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	int size = PyList.Size(p);
+	
+	printf("[*] Size of the Python List = %d", size);
+	printf("[*] Allocated = %d", Py_SIZE(p));
+	for(int i = 0; i < size; i++)
+	{
+		print("Element %d: %s", i, Py_TYPE(PyList_GetItem(p, 1)))
+	}
+
+}
