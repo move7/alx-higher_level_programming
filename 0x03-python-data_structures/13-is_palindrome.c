d2024623319386b86aa6906a63811708fa4fedc2
move7 <addouni.fatima@gmail.com>
2023-06-12 13:15:28 -0700

    task13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,33 +1,38 @@
 #include "lists.h"
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
+ */
 int is_palindrome(listint_t **head)
 {
 	if (*head == NULL || (*head)->next == NULL)
-		return(1);
+		return (1);
 	listint_t *fast = *head;
 	listint_t *slow = *head;
 	listint_t *prev = NULL;
 	listint_t *next = NULL;
 	listint_t *head_dup = *head;
 
-	while(fast->next && fast->next->next)
+	while (fast->next && fast->next->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 	}
-	while(slow->next)
+	while (slow->next)
 	{
 		next = slow->next;
 		slow->next = prev;
 		prev = slow;
 		slow = next;
 	}
-	while(prev && head_dup)
+	while (prev && head_dup)
 	{
 		if (prev->n != head_dup->n)
-			return(0);
+			return (0);
 		prev = prev->next;
 		head_dup = head_dup->next;
 	}
-	return(1);
-	
+	return (1);
 }
