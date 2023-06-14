78b46f6de77a65f5a8104f821d06c7f9ddf3133f
move7 <addouni.fatima@gmail.com>
2023-06-14 13:07:07 -0700

    task2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,3 @@
 #!/usr/bini/python3
 def uniq_add(my_list=[]):
-    sum_uniq = 0
-    for i in range(len(my_list)):
-        if i == my_list.index(my_list[i]):
-            sum_uniq += my_list[i]
-    return sum_uniq
+    return sum(set(my_list))
