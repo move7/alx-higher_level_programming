48a66d63c230ce4157eaaf1d0d0f4342c725caca
move7 <addouni.fatima@gmail.com>
2023-06-14 13:01:00 -0700

    tasks

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,7 +2,7 @@
 def common_elements(set_1, set_2):
     if len(set_1) > len(set_2):
         set_1, set_2 = set_2, set_1
-    set_common=set()
+    set_common = set()
     for i in set_1:
         if i in set_2 and i not in set_common:
             set_common.add(i)
