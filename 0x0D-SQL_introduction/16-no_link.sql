6ed93460896ea22a8d868769b2eb0c3439e1f4e7
move7 <addouni.fatima@gmail.com>
2023-08-15 12:40:30 -0700

    tasks mandatory

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+--  List all records of the table second_table of the database hbtn_0c_0 in  MySQL server.
+SELECT `score` AS score, `name` AS name FROM `second_table`;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- display the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in  MySQL serve.
+SELECT COUNT(`id`) FROM `first_table`;
